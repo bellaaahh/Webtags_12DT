a0fe15a5a48ee383e9fbc9899dda64f82ec90b04
bellaaahh <126836491+bellaaahh@users.noreply.github.com>
2023-06-13 00:08:02 +0000

    moved html from static to dynamic

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask! test'
+    return render_template("index.html")
 
+
+@app.route('/webpages')
+def render_webpages():
+  return render_template('webpages.html')
+
+@app.route('/styles')
+def render_styles():
+  return render_template('styles.html')
 
 app.run(host='0.0.0.0', port=81)
